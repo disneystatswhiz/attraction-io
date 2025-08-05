fa4b17704db4bad96556a5066389449cf8290ea1
DisneyStatsWhiz <fred@touringplans.com>
2025-08-05 13:36:13 -0400

    Use --project=. in pipeline.sh to force Julia project env

--- pipeline.sh
+++ pipeline.sh
@@ -7,7 +7,7 @@ cd /home/ubuntu/attraction-io
 git pull
 
 # Run Julia pipeline (change path to your Julia bin if not in PATH)
-julia scheduler/run_jobs.jl
+julia --project=. scheduler/run_jobs.jl
 
 # Auto-shutdown after run (stops the instance to save cost, not terminate)
 # sudo shutdown -h now
