fdcf1d9dfc35f23a0e1569f82eb78d4a995719b1
DisneyStatsWhiz <fred@touringplans.com>
2025-08-09 15:54:16 -0400

    Adjusting pipeline.sh for dynamically selecting AWS region

--- pipeline.sh
+++ pipeline.sh
@@ -9,13 +9,22 @@ git reset --hard HEAD
 git pull
 
 stop_instance() {
-  TOKEN=$(curl -sS -X PUT "http://169.254.169.254/latest/api/token" \
+    # IMDSv2 token
+    TOKEN=$(curl -sS -X PUT "http://169.254.169.254/latest/api/token" \
     -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-  IID=$(curl -sS -H "X-aws-ec2-metadata-token: $TOKEN" \
+
+    # Instance ID + AZ from metadata
+    IID=$(curl -sS -H "X-aws-ec2-metadata-token: $TOKEN" \
     http://169.254.169.254/latest/meta-data/instance-id)
-  REG=$(curl -sS -H "X-aws-ec2-metadata-token: $TOKEN" \
-    http://169.254.169.254/latest/dynamic/instance-identity/document | awk -F\" '/region/ {print $4}')
-  aws ec2 stop-instances --instance-ids "$IID" --region "$REG" || true
+
+    AZ=$(curl -sS -H "X-aws-ec2-metadata-token: $TOKEN" \
+    http://169.254.169.254/latest/meta-data/placement/availability-zone)
+
+    # Region = AZ without the trailing letter (e.g., "us-east-1c" -> "us-east-1")
+    REG=${AZ::-1}
+
+    echo "Stopping instance $IID in region $REG ..."
+    aws ec2 stop-instances --instance-ids "$IID" --region "$REG"
 }
 
 trap 'sleep 5; stop_instance' EXIT
