526e4de2aa585b59df75707638968a1aec794256
DisneyStatsWhiz <fred@touringplans.com>
2025-09-02 09:30:06 -0400

    Test on Sept 1 2025 showed that 8 is the max value of MAX_PARALLEL_PER_GROUP that the instance will support

--- monitor-ec2.sh
+++ monitor-ec2.sh
@@ -4,4 +4,4 @@ aws ssm start-session \
   --profile stats \
   --target i-0f24187fd8c69a39f \
   --document-name AWS-StartInteractiveCommand \
-  --parameters "command=[\"sudo -iu ubuntu bash -lc 'cd /home/ubuntu/attraction-io/logs && tail -f pipeline_latest.log'\"]"
+  --parameters "command=[\"sudo -iu ubuntu bash -lc 'cd /home/ubuntu/attraction-io/logs && tail -n +1 -f pipeline_latest.log'\"]"
