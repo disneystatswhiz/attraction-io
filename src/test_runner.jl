f2e0616f7c8755e2444595a4bdce436ebe026370
DisneyStatsWhiz <fred@touringplans.com>
2025-10-24 17:08:46 -0400

    Simplified pipeline.sh - running only fact table update for now

--- src/test_runner.jl
+++ src/test_runner.jl
@@ -1,74 +1,7 @@
 # ===================================================================================== #
-# -------------------------- Parse and Validate Arguments ----------------------------- #
+# ------------------------------ Attraction IO Runner --------------------------------- #
 # ===================================================================================== #
 
-#if length(ARGS) < 4
-#    # @error "❌ ARGS missing. Usage: julia main_runner.jl <entity> <park> <property> <type>"
-#    exit(1)
-#end
-
-const ENTITY_CODE = "ak01"
-#const PARK        = "ak"
-#const PROPERTY    = "wdw"
-#const QUEUE_TYPE  = "standby"
-
-# ===================================================================================== #
-# ------------------------- Initial Setup and Modules --------------------------------- #
-# ===================================================================================== #
-ROOT = abspath(joinpath(@__DIR__, ".."))
-start_time_pipeline = time_ns()
-
-include(joinpath(ROOT, "src", "utilities", "utility_setup.jl"))
-include(joinpath(ROOT, "src", "modules", "mod_encoders.jl"))
-using .EncodeFeatures
-
-# ===================================================================================== #
-# -------------------- Preprocessing & Feature Engineering ---------------------------- #
-# ===================================================================================== #
-
-include(joinpath(ROOT, "src", "data", "run_set_attraction.jl"))
-include(joinpath(ROOT, "src", "data", "run_sync.jl"))
-include(joinpath(ROOT, "src", "data", "run_wait_time_ingestion.jl"))
-include(joinpath(ROOT, "src", "data", "run_futuredates.jl"))
-include(joinpath(ROOT, "src", "data", "run_features.jl"))
-include(joinpath(ROOT, "src", "data", "run_premodelling.jl"))
-
-# ===================================================================================== #
-# -------------------------- Forecasting + Scoring ------------------------------------ #
-# ===================================================================================== #
-
-include(joinpath(ROOT, "src", "modelling", "run_encodefeatures.jl"))
-include(joinpath(ROOT, "src", "modelling", "run_trainer.jl"))
-include(joinpath(ROOT, "src", "modelling", "run_predictions.jl"))
-# include(joinpath(ROOT, "src", "modelling", "run_writer.jl"))  # Optional S3 writer
-
-# ===================================================================================== #
-# ------------------------ Crowd Calendar Assignment ---------------------------------- #
-# ===================================================================================== #
-
-include(joinpath(ROOT, "src", "calendar", "run_dailyavgs.jl"))
-include(joinpath(ROOT, "src", "calendar", "run_thresholds.jl"))
-include(joinpath(ROOT, "src", "calendar", "run_assign_levels.jl"))
-include(joinpath(ROOT, "src", "calendar", "run_observed_dailyavgs.jl"))
-
-# ===================================================================================== #
-# ----------------------------- Reporting Outputs ------------------------------------- #
-# ===================================================================================== #
-
-
-include(joinpath(ROOT, "src", "reporting", "run_descriptives.jl"))
-include(joinpath(ROOT, "src", "reporting", "run_accuracyreports.jl"))
-include(joinpath(ROOT, "src", "reporting", "run_daily_wait_time_curve.jl"))
-
-# ===================================================================================== #
-# ---------------------------- Final Logging and Cleanup ------------------------------ #
-# ===================================================================================== #
-
-elapsed_modelling = (time_ns() - start_time_pipeline) / 1e9
-log_header("✅ Modelling completed for $(ATTRACTION.code) - $(ATTRACTION.name) in $(round(elapsed_modelling / 60, digits=2)) minutes.")
-
-if !(ATTRACTION.code in [ENTITY_CODE])
-    cleanup_folders(ATTRACTION.code, base_dir=ROOT)
-end
-# ====================================================================================== #
-# ----------------------------- End of Main Runner Script ------------------------------ #
+include("../src/main_runner.jl")
+include("../src/main_setup.jl")  # ensures Main.DATA_FACT
+run_entity("AK86"; data_fact=Main.DATA_FACT)
