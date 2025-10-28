c5cfeeb8ad4d22954f4be88ce0bd21d888394481
DisneyStatsWhiz <fred@touringplans.com>
2025-10-28 14:36:47 -0400

    Cleaned up unused scripts

--- monitor-ec2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-aws ssm start-session \
-  --region us-east-1 \
-  --profile stats \
-  --target i-0f24187fd8c69a39f \
-  --document-name AWS-StartInteractiveCommand \
-  --parameters "command=[\"sudo -iu ubuntu bash -lc 'cd /home/ubuntu/attraction-io/logs && tail -n +1 -f pipeline_latest.log'\"]"
